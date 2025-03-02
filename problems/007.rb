ebc5c8174f9501ac3e0ec47db8136cffe1a7b716
papaya <71032999+esmaycat@users.noreply.github.com>
2025-03-02 17:00:50 +0000

    unify prime detection

--- problems/007.rb
+++ problems/007.rb
@@ -2,7 +2,7 @@ prime = lambda { |n|
   return n != 1 if n < 4
   return false if (n % 3).zero? || n.even?
 
-  5.step(n**0.5, 6).map { |i| (n % i).zero? || (n % (i + 2)).zero? }.none?
+  5.step(n**0.5, 6).all? { |i| (n % i) != 0 && (n % (i + 2)) != 0 }
 }
 
 puts (1..).lazy.filter(&prime).drop(10_000).first
