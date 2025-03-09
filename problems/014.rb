d659ca3edca97b4ec5755a68630f7a53fa0afd23
papaya <71032999+esmaycat@users.noreply.github.com>
2025-03-09 19:32:49 +0000

    problem 14

---
+++ problems/014.rb
@@ -0,0 +1,10 @@
+collatz = lambda { |n|
+  @memo ||= {}
+  return @memo[n] if @memo.key? n
+  return 1 if n == 1
+
+  chain = collatz[n.even? ? n / 2 : (3 * n) + 1]
+  @memo[n] = chain + 1
+}
+
+p((1..1_000_000).max_by(&collatz))
