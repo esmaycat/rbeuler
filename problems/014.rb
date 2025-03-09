5063caffab271167b425e2905002b6d2ea591c4b
papaya <71032999+esmaycat@users.noreply.github.com>
2025-03-09 19:49:47 +0000

    move guard clause to hashmap

--- problems/014.rb
+++ problems/014.rb
@@ -1,7 +1,6 @@
 collatz = lambda { |n|
-  @memo ||= {}
+  @memo ||= { 1 => 1 }
   return @memo[n] if @memo.key? n
-  return 1 if n == 1
 
   chain = collatz[n.even? ? n / 2 : (3 * n) + 1]
   @memo[n] = chain + 1
