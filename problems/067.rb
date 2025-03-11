b2faed42e07d47d1c86476652bb91429a44d1869
papaya <71032999+esmaycat@users.noreply.github.com>
2025-03-11 23:26:09 +0000

    fix symlink

--- problems/067.rb
+++ problems/067.rb
@@ -1,1 +1,9 @@
-problems/018.rb
+# Expect input on stdin
+triangle = $stdin.each_line.map { _1.chomp.split.map(&:to_i) }
+(triangle.length - 2).downto(0) do |n|
+  below = triangle[n + 1]
+  triangle[n].each_index do |node|
+    triangle[n][node] += [below[node], below[node + 1]].max
+  end
+end
+puts(triangle[0][0])
