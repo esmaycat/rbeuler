521604a6f6270483a6fece67f91878c7873b9b69
papaya <71032999+esmaycat@users.noreply.github.com>
2025-03-11 23:11:10 +0000

    switch up problem 11 slightly

--- problems/011.rb
+++ problems/011.rb
@@ -2,9 +2,9 @@
 ns = $stdin.each_line.map { _1.chomp.split.map(&:to_i) }
 puts((0..19).to_a.repeated_permutation(2).flat_map do |y, x|
   [
-    x <= 16 ? ns[y][x, 4] : nil,
-    y <= 16 ? ns[y, 4].map { _1[x] } : nil,
-    x <= 16 && y <= 16 ? ns[y, 4].each_index.map { ns[y + _1][x + _1] } : nil,
-    x >= 4 && y <= 16 ? ns[y, 4].each_index.map { ns[y + _1][x - _1] } : nil
-  ].compact.map { _1.reduce :* }
+    x <= 16 && ns[y][x, 4],
+    y <= 16 && ns[y, 4].map { _1[x] },
+    x <= 16 && y <= 16 && ns[y, 4].each_index.map { ns[y + _1][x + _1] },
+    x >= 4 && y <= 16 && ns[y, 4].each_index.map { ns[y + _1][x - _1] }
+  ].filter_map { _1.reduce :* if _1 }
 end.max)
